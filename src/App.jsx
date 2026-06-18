6f331f2aa25e0a254c03bc8a3760c3b9245f0776
Gauvain-Pataky1 <Gauvain.Pataky@kyndryl.com>
2026-06-18 20:07:00 +0200

    Age jack better cards + cheat route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { GameProvider } from './context/GameContext';
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { GameProvider, useGame } from './context/GameContext';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
@@ -8,6 +9,16 @@ import MyZoo from './pages/MyZoo';
 import Play from './pages/Play';
 import Breed from './pages/Breed';
 import './App.css';
+
+function CheatPage() {
+  const { ageJackReward } = useGame();
+  const navigate = useNavigate();
+  useEffect(() => {
+    ageJackReward(10000);
+    navigate('/');
+  }, [ageJackReward, navigate]);
+  return null;
+}
 
 function App() {
   return (
@@ -22,6 +33,7 @@ function App() {
             <Route path="/zoo" element={<MyZoo />} />
             <Route path="/play" element={<Play />} />
             <Route path="/breed" element={<Breed />} />
+            <Route path="/neginmylife" element={<CheatPage />} />
           </Routes>
         </div>
       </GameProvider>
